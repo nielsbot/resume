508b326ee155813a787830488018d5349e0aebc7
Niels Gabel <nielsgabel@nielsmbp.local>
2019-10-25 18:33:59 -0700

    typos… yikes!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,24 +25,24 @@ const nielsgabel: ResumeData = {
             title: 'Design Tools — Human Interface',
             company: `Apple Inc`,
             description: [
-                `Lead programmer on Apple's primary UI design tool. directly supporting the UI prototyping team.` +
-                `\n\nAlso:` +
-                `\n\nBuilt a full-featured JS-to-native library similar to [PyObjC](https://pypi.org/project/pyobjc/). Includes subclassing Obj-C from JavaScript, support for Obj-C in-out arguments, and more.` +
-                `\n\nBuilt a prototype visual programming language. Nodes can be created using Swift directly in the editor--the code is dynamically re-compiled.`
+                `Lead programmer on Apple's primary UI design tool, directly supporting the UI prototyping team.` +
+                `\n\n_Also:_` +
+                `\n\nBuilt a full-featured JS-to-native library similar to [PyObjC](https://pypi.org/project/pyobjc/). Supports subclassing Obj-C from JavaScript, Obj-C in-out arguments, and more.` +
+                `\n\nBuilt a prototype visual programming language. Supports live editing of Swift via dynamic recompile and reload.`
             ],
             when: `2012 Apr — Now`
         },
         {
             title: `Design and Technology`,
             company: `Seesaw Psychology Group`,
-            description: `Co-founder. Responsibilities include: website, [interior design](https://www.icloud.com/sharedalbum/#B1aJ5HumnJJshVM), custom internal apps, and more.`,
+            description: `Co-founder. Responsible for design ([interiors](https://www.icloud.com/sharedalbum/#B1aJ5HumnJJshVM), marketing collateral, and website), and internal apps, and more.`,
             when: `2010 Aug — Now`
         },
         {
             title: 'Lead iOS Developer',
             company: `Doubledutch Inc`,
             description: [
-                `Designed and built enterprise iOS apps. Designed a built a news app (think Apple News) for Cisco's sales team.`
+                `Design, implementation and maintenance of enterprise iOS apps. Designed and built a server-configurable iPad news reader for a large enterprise sales team.`
             ],
             when: `2011 Dec — 2012 Apr`
         },
@@ -50,21 +50,21 @@ const nielsgabel: ResumeData = {
             title: 'Designer and iOS developer',
             company: `Spot Inc`,
             description: [
-                `Designed and built of Spot, a location wishlisting app a highly visual and compelling user experience.`
+                `Designed and built Spot, a design-focused location wishlisting app with a polished and compelling user experience.`
             ],
             when: `2010 Aug — 2011 Dec`
         },
         {
             title: 'Staff Engineer',
             company: `Sun Microsystems`,
-            description: `Worked on a visual programming environment for JavaFX`,
+            description: `Was part of the team building a visual programming environment for JavaFX`,
             when: `2008 Nov — 2010 Jul`
         },
         {
             title: 'Independent Software Developer',
             description: [
                 `IOS App consulting, design and programming.`,
-                `Co-founded HoodHot. We designed and built Tokyo Teleport, an iOS travel guide that incorporated professionally produced video segments, offline maps, and more.`
+                `Co-founded HoodHot Inc. Designed and built Tokyo Teleport, an iOS travel guide to Tokyo. Really, a pocketable travel show incorporating professionally produced video segments, offline maps, and more.`
             ],
             when: `2007 Oct — 2008 Sep`
         },
@@ -74,10 +74,10 @@ const nielsgabel: ResumeData = {
             when: `2007 Aug — 2007 Oct`
         },
         {
-            title: 'Senior Scientist/Engineer — Core OS',
+            title: 'Senior Scientist/Engineer',
             company: `Apple Inc`,
             description: [
-                `Wrote and maintained the FireWire user client. FireWire I/O Kit driver developer. Learned a lot about professional software development and multithreaded and asynchronous software.`
+                `Built and maintained the FireWire user client. Helped develop the FireWire driver stack for OS X. Learned a lot about professional software development and multithreaded and asynchronous software.`
             ],
             when: `1999 Oct — 2007 Jul`
         },
